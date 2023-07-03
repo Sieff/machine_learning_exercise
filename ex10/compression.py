b4b93c51f7df560aa40766f18fb45dc7995d49ee
Steffen Schubert <steff.schubert@gmx.de>
2023-07-03 19:21:26 +0200

    Ex10

--- ex10/compression.py
+++ ex10/compression.py
@@ -1,4 +1,26 @@
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import cv2
+import numpy as np
+
+
+def reduce_dimension(X):
+    pca = PCA(n_components=2).fit(X)
+
+    pca_transform = pca.transform(X)
+
+    plt.scatter(pca_transform[:, 0], pca_transform[:, 1])
+    plt.title("PCA")
+    plt.show()
+
+    mds_transform = MDS(n_components=2).fit_transform(X)
+
+    plt.scatter(mds_transform[:, 0], mds_transform[:, 1])
+    plt.title("MDS")
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -9,16 +31,16 @@ if __name__ == '__main__':
     plt.figure().add_subplot(111, projection = '3d').plot(*X.T, 'o')
     plt.show()
 
-    ...
-    
+    reduce_dimension(X)
+
     X = np.load('./spring_1.npy')
     plt.figure().add_subplot(111, projection = '3d').plot(*X.T, 'o')
     plt.show()
 
-    ...
+    reduce_dimension(X)
     
     X = np.load('./spring_2.npy')
     plt.figure().add_subplot(111, projection = '3d').plot(*X.T, 'o')
     plt.show()
 
-    ...
+    reduce_dimension(X)
